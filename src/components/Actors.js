2979cbe23e5901c35b07d9c6775be7c37717ae26
Nziza21 <144722415+Nziza21@users.noreply.github.com>
2023-11-03 00:48:38 +0300

    Update Actors.js

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { actors } from "../data";
+import { actors } from "../data"; // Import the actors data
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h2>Name: {actor.name}</h2>
+          <h3>Movies Acted In:</h3>
+          <ul>
+            {actor.movies.map((movie, movieIndex) => (
+              <li key={movieIndex}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
